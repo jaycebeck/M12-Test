0f1e96bd6e76476736c83289ba20ace5a356e5bd
jaycehold <jaycehold@gmail.com>
2024-03-02 17:20:31 -0700

    First commit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,11 +7,11 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
 
 // Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+//  https://firebase.google.com/docs/functions/typescript
 
 // export const helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
